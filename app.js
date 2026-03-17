2d531e9420e8e29e53e71ab29937361edbe51883
Roberto Hernandez Cruz <240396@utxicotepec.edu.mx>
2026-03-17 11:52:24 -0600

    add: se agragaron dependencias y esas cosas B)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const http = require('body-parser');
+const http = require('http');
 const app = express();
 
 app.use(logger('dev'));
@@ -11,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Bienvenido a mi API de tienda virtual' 
 }));
+
+const port = parseInt(process.env.PORT,10) || 8000;
+app.set('port', port )
 
 const server = http.createServer(app);
 server.listen(port);
