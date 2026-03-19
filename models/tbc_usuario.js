f4ebf5269361405828ae4343ee3d140c344eb415
Roberto Hernandez Cruz <240396@utxicotepec.edu.mx>
2026-03-19 14:29:16 -0600

    Add: se añadieron los modelos de tablas d la nueva base de datos

---
+++ models/tbc_usuario.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class tbc_usuario extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tbc_usuario.init({
+    nombre: { type: DataTypes.STRING(100), allowNull: false },
+    direccion: { type: DataTypes.STRING(200), allowNull: false },
+    telefono: { type: DataTypes.STRING(15), allowNull: false },
+    email: { type: DataTypes.STRING(100), allowNull: false },
+    password: { type: DataTypes.STRING(100), allowNull: false },
+    rol: { type: DataTypes.ENUM('cliente', 'administrador'), allowNull: false, defaultValue: 'cliente' },
+    fecha_registro: { type: DataTypes.DATE, allowNull: false }
+  }, {
+    sequelize,
+    modelName: 'tbc_usuario',
+  });
+  return tbc_usuario;
+};
